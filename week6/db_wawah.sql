9dfcd5c4c91a7ba6380cc1baea6b332db056e413
Wawah Sri Sunenti <142108899+wawahsrisunenti@users.noreply.github.com>
2023-09-25 15:50:37 +0700

    buat tabel barang

--- week6/db_wawah.sql
+++ week6/db_wawah.sql
@@ -2,3 +2,12 @@ CREATE TABLE public.kategori_barang
 (	id SERIAL PRIMARY KEY,
     nama_kategori VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE public.barang (
+    id SERIAL PRIMARY KEY,
+    nama_barang VARCHAR(255) NOT NULL,
+    harga_barang DECIMAL(10, 2) NOT NULL,
+    stok INT NOT NULL,
+    keterangan TEXT,
+    id_kategori INT REFERENCES kategori_barang(id)
+);
