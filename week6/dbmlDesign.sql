abca1e926ce00771644724c8750f2f5c5cf96b82
Wawah Sri Sunenti <142108899+wawahsrisunenti@users.noreply.github.com>
2023-09-25 15:45:40 +0700

    update table barang
    menambahkan harga, stok, dan keterangan

--- week6/dbmlDesign.sql
+++ week6/dbmlDesign.sql
@@ -6,5 +6,7 @@ Table kategori_barang {
 Table barang {
   id int [primary key]
   nama_barang varchar(255) [not null]
-  
+  harga_barang decimal(10, 2) [not null]
+  stok int [not null]
+  keterangan text
 }
