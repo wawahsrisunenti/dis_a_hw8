b1e4aeedaf9fcc98285dbef4250468fb86cb85a4
Wawah Sri Sunenti <142108899+wawahsrisunenti@users.noreply.github.com>
2023-09-25 15:52:30 +0700

    make a query for kategori_barang
    using select, insert and update

--- week6/db_wawah.sql
+++ week6/db_wawah.sql
@@ -11,3 +11,24 @@ CREATE TABLE public.barang (
     keterangan TEXT,
     id_kategori INT REFERENCES kategori_barang(id)
 );
+
+SELECT * FROM kategori_barang;
+
+INSERT INTO kategori_barang (nama_kategori)
+VALUES ('Mainan Anak-anak'),
+       ('Mainan Edukatif'),
+       ('Mainan Elektronik'),
+	   ('Mainan Anak Laki-laki'),
+	   ('Mainan Anak Perempuan'),
+	   ('Mainan Balita'),
+	   ('Mainan Aktivitas Luar');
+
+//Mengubah nama kategori dengan ID 1 menjadi "Mainan Anak"
+UPDATE kategori_barang
+SET nama_kategori = 'Mainan Anak'
+WHERE id = 1;
+
+//Mengubah nama kategori dengan ID 2 menjadi "Mainan Edukasi"
+UPDATE kategori_barang
+SET nama_kategori = 'Mainan Edukasi'
+WHERE id = 2;
