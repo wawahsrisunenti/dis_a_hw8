94b929174d1154bbf487c14a53b4f981ad393ecf
Wawah Sri Sunenti <142108899+wawahsrisunenti@users.noreply.github.com>
2023-10-04 14:04:45 +0700

    make module
    untuk menghitung luas dan keliling persegi dan persegi panjang

---
+++ wawahsrisunentiHw8/modGeometry.js
@@ -0,0 +1,6 @@
+module.exports = {
+  hitungLuasPersegi: (sisi) => sisi * sisi,
+  hitungKelilingPersegi: (sisi) => 4 * sisi,
+  hitungLuasPersegiPanjang: (panjang, lebar) => panjang * lebar,
+  hitungKelilingPersegiPanjang: (panjang, lebar) => 2 * (panjang + lebar),
+};
