b219a98e5ece1b73fe1f92e54a1beb5bfb2c8304
Wawah Sri Sunenti <142108899+wawahsrisunenti@users.noreply.github.com>
2023-10-04 14:06:27 +0700

    make soal1.js
    ini file utama yang memanggil modul modGeometry.js

---
+++ wawahsrisunentiHw8/question1.js
@@ -0,0 +1,12 @@
+// soal1.js
+
+const geometry = require('./geometry');
+
+const sisiPersegi = 5;
+console.log(`Luas Persegi dengan sisi ${sisiPersegi} adalah ${geometry.hitungLuasPersegi(sisiPersegi)}`);
+console.log(`Keliling Persegi dengan sisi ${sisiPersegi} adalah ${geometry.hitungKelilingPersegi(sisiPersegi)}`);
+
+const panjangPersegiPanjang = 6;
+const lebarPersegiPanjang = 4;
+console.log(`Luas Persegi Panjang dengan panjang ${panjangPersegiPanjang} dan lebar ${lebarPersegiPanjang} adalah ${geometry.hitungLuasPersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang)}`);
+console.log(`Keliling Persegi Panjang dengan panjang ${panjangPersegiPanjang} dan lebar ${lebarPersegiPanjang} adalah ${geometry.hitungKelilingPersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang)}`);
